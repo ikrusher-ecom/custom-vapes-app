6d97e8f2ea69589c2976ed0e27ddb5c094162d3c
kikijinqili <jinqi.li.310@gmail.com>
2022-04-14 02:15:57 -0700

    4-13-2022 input color

--- pages/components/widgets/AstroModWidget.js
+++ pages/components/widgets/AstroModWidget.js
@@ -2,7 +2,7 @@
  * @Author: Jinqi Li
  * @Date: 2022-04-13 17:37:17
  * @LastEditors: Jinqi Li
- * @LastEditTime: 2022-04-14 01:33:20
+ * @LastEditTime: 2022-04-14 02:12:36
  * @FilePath: /custom-vapes-app/pages/components/widgets/AstroModWidget.js
  */
 import * as React from 'react';
@@ -13,6 +13,11 @@ import CustomImages from '../CustomImages';
 import { Button } from '@mui/material';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 export default function AstroModWidget(props) {
 	const productID = 'astro-mod';
@@ -108,6 +113,17 @@ export default function AstroModWidget(props) {
 		}
 	};
 
+	const formStyles = {
+		fontSize: '16px',
+		padding: '20px 10px 10px'
+	};
+
+	const [ prodColor, setProdColor ] = useState('black');
+
+	const handleChangeColor = (event) => {
+		setProdColor(event.target.value);
+	};
+
 	return (
 		<div className="custom-vape">
 			<div className={'next-btn ' + styles.angleBtns}>
@@ -131,147 +147,241 @@ export default function AstroModWidget(props) {
 						}}
 					>
 						<div id="oneAM" style={{ display: num === 1 ? 'block' : 'none' }}>
-							{productColors.map((color) => {
-								return (
-									<CustomImages
-										name={productID}
-										category={productCate}
-										folder={productFolder}
-										images={productImages[color][0]}
-										color={color}
-										key={productID + color}
-									/>
-								);
-							})}
+							{console.log(productColors.filter((color) => color === prodColor))}
+							<CustomImages
+								name={productID}
+								category={productCate}
+								folder={productFolder}
+								images={productImages[productColors.filter((color) => color === prodColor)][0]}
+							/>
 						</div>
 						<div id="twoAM" style={{ display: num === 2 ? 'block' : 'none' }}>
-							{productColors.map((color) => {
-								return (
-									<CustomImages
-										name={productID}
-										category={productCate}
-										folder={productFolder}
-										images={productImages[color][1]}
-										color={color}
-										key={productID + color}
-									/>
-								);
-							})}
+							<CustomImages
+								name={productID}
+								category={productCate}
+								folder={productFolder}
+								images={productImages[productColors.filter((color) => color === prodColor)][1]}
+							/>
 						</div>
 						<div id="threeAM" style={{ display: num === 3 ? 'block' : 'none' }}>
-							{productColors.map((color) => {
-								return (
-									<CustomImages
-										name={productID}
-										category={productCate}
-										folder={productFolder}
-										images={productImages[color][2]}
-										color={color}
-										key={productID + color}
-									/>
-								);
-							})}
+							<CustomImages
+								name={productID}
+								category={productCate}
+								folder={productFolder}
+								images={productImages[productColors.filter((color) => color === prodColor)][2]}
+							/>
 						</div>
 						<div id="fourAM" style={{ display: num === 4 ? 'block' : 'none' }}>
-							{productColors.map((color) => {
-								return (
-									<CustomImages
-										name={productID}
-										category={productCate}
-										folder={productFolder}
-										images={productImages[color][3]}
-										color={color}
-										key={productID + color}
-									/>
-								);
-							})}
+							<CustomImages
+								name={productID}
+								category={productCate}
+								folder={productFolder}
+								images={productImages[productColors.filter((color) => color === prodColor)][3]}
+							/>
 						</div>
 						<div id="fiveAM" style={{ display: num === 5 ? 'block' : 'none' }}>
-							{productColors.map((color) => {
-								return (
-									<CustomImages
-										name={productID}
-										category={productCate}
-										folder={productFolder}
-										images={productImages[color][4]}
-										color={color}
-										key={productID + color}
-									/>
-								);
-							})}
+							<CustomImages
+								name={productID}
+								category={productCate}
+								folder={productFolder}
+								images={productImages[productColors.filter((color) => color === prodColor)][4]}
+							/>
 						</div>
 						<div id="sixAM" style={{ display: num === 6 ? 'block' : 'none' }}>
-							{productColors.map((color) => {
-								return (
-									<CustomImages
-										name={productID}
-										category={productCate}
-										folder={productFolder}
-										images={productImages[color][5]}
-										color={color}
-										key={productID + color}
-									/>
-								);
-							})}
+							<CustomImages
+								name={productID}
+								category={productCate}
+								folder={productFolder}
+								images={productImages[productColors.filter((color) => color === prodColor)][5]}
+							/>
 						</div>
 						<div id="sevenAM" style={{ display: num === 7 ? 'block' : 'none' }}>
-							{productColors.map((color) => {
-								return (
-									<CustomImages
-										name={productID}
-										category={productCate}
-										folder={productFolder}
-										images={productImages[color][6]}
-										color={color}
-										key={productID + color}
-									/>
-								);
-							})}
+							<CustomImages
+								name={productID}
+								category={productCate}
+								folder={productFolder}
+								images={productImages[productColors.filter((color) => color === prodColor)][6]}
+							/>
 						</div>
 						<div id="eightAM" style={{ display: num === 8 ? 'block' : 'none' }}>
-							{productColors.map((color) => {
-								return (
-									<CustomImages
-										name={productID}
-										category={productCate}
-										folder={productFolder}
-										images={productImages[color][7]}
-										color={color}
-										key={productID + color}
-									/>
-								);
-							})}
+							<CustomImages
+								name={productID}
+								category={productCate}
+								folder={productFolder}
+								images={productImages[productColors.filter((color) => color === prodColor)][7]}
+							/>
 						</div>
 						<div id="nineAM" style={{ display: num === 9 ? 'block' : 'none' }}>
-							{productColors.map((color) => {
-								return (
-									<CustomImages
-										name={productID}
-										category={productCate}
-										folder={productFolder}
-										images={productImages[color][8]}
-										color={color}
-										key={productID + color}
-									/>
-								);
-							})}
+							{/* {productColors.map((color) => {
+								return ( */}
+							<CustomImages
+								name={productID}
+								category={productCate}
+								folder={productFolder}
+								images={productImages[productColors.filter((color) => color === prodColor)][8]}
+							/>
 						</div>
 						<div id="tenAM" style={{ display: num === 10 ? 'block' : 'none' }}>
-							{productColors.map((color) => {
-								return (
-									<CustomImages
-										name={productID}
-										category={productCate}
-										folder={productFolder}
-										images={productImages[color][9]}
-										color={color}
-										key={productID + color}
-									/>
-								);
-							})}
+							<CustomImages
+								name={productID}
+								category={productCate}
+								folder={productFolder}
+								images={productImages[productColors.filter((color) => color === prodColor)][9]}
+							/>
 						</div>
 					</div>
 				</div>
+			</div>
+			<div className="input-group">
+				<table>
+					<tbody>
+						<tr>
+							<th>Available Colors:</th>
+							<td>
+								<Box sx={{ width: 300 }}>
+									<FormControl fullWidth style={formStyles}>
+										<InputLabel id="color-select-label" style={formStyles}>
+											Color
+										</InputLabel>
+										<Select
+											labelId="color-select-label"
+											id="color-select"
+											value={prodColor}
+											label="Color"
+											onChange={handleChangeColor}
+											style={{ fontSize: '16px' }}
+											defaultValue={'black'}
+										>
+											<MenuItem style={formStyles} value={'black'} selected>
+												Black
+											</MenuItem>
+											<MenuItem style={formStyles} value={'rosegold'}>
+												Rose Gold
+											</MenuItem>
+											<MenuItem style={formStyles} value={'red'}>
+												Red
+											</MenuItem>
+											<MenuItem style={formStyles} value={'metablue'}>
+												Metallic Blue
+											</MenuItem>
+											<MenuItem style={formStyles} value={'lilac'}>
+												Lilac
+											</MenuItem>
+											<MenuItem style={formStyles} value={'gunmetal'}>
+												Gunmetal
+											</MenuItem>
+										</Select>
+									</FormControl>
+								</Box>
+							</td>
+						</tr>
+						{/* <tr className="custom-logo-div" id="upload-logo-oneAS" style="height: 150px;">
+							<th>Upload Logo:</th>
+							<td>
+								<p>
+									<input
+										type="file"
+										accept="image/png"
+										name="image"
+										id="file"
+										onChange="loadFile(event)"
+										style="display: none;"
+									/>
+								</p>
+								<p>
+									<label className="logo-button" htmlFor="file" style="cursor: pointer;">
+										Upload Logo
+									</label>
+								</p>
+								<p style="font-size: 12px;">
+									Click the button above to select a logo file from your computer or other file
+									storage. We accept PNG FILE and FILE SIZE not more than 1 MB only.
+								</p>
+							</td>
+						</tr>
+						<tr style="height: 50px;">
+							<th>Customize Text:</th>
+							<td>
+								<div className="custom-text-div" id="input-text-oneAS">
+									<input
+										type="text"
+										id="custom-text-oneAS"
+										maxLength="15"
+										placeholder="Less than 15 characters"
+										onInput="onTextInputOne()"
+									/>
+								</div>
+								<div className="custom-text-div" id="input-text-threeAS">
+									<input
+										type="text"
+										id="custom-text-threeAS"
+										maxLength="15"
+										placeholder="Less than 15 characters"
+										onInput="onTextInputThree()"
+									/>
+								</div>
+								<div className="custom-text-div" id="input-text-fiveAS">
+									<input
+										type="text"
+										id="custom-text-fiveAS"
+										maxLength="15"
+										placeholder="Less than 15 characters"
+										onInput="onTextInputFive()"
+									/>
+								</div>
+								<div className="custom-text-div" id="input-text-sevenAS">
+									<input
+										type="text"
+										id="custom-text-sevenAS"
+										maxLength="15"
+										placeholder="Less than 15 characters"
+										onInput="onTextInputSeven()"
+									/>
+								</div>
+								<input type="color" id="pickerAS" name="picker" value="#808080" />
+							</td>
+						</tr>
+						<tr>
+							<th>Need Custom User Manual? </th>
+							<td style="padding-left: 1em; vertical-align: middle;">
+								<input type="radio" id="manual-yesAS" name="manual" value="manual-yes" />
+								<label htmlFor="manual-yes">Yes</label>
+								<input type="radio" id="manual-noAS" name="manual" value="manual-no" />
+								<label htmlFor="manual-no">No</label>
+							</td>
+						</tr>
+						<tr>
+							<th>Need Custom Packaging or Shipping Carton? </th>
+							<td style="padding-left: 1em; vertical-align: middle;">
+								<input type="radio" id="package-yesAS" name="package" value="package-yes" />
+								<label htmlFor="package-yes">Yes</label>
+								<input type="radio" id="package-noAS" name="package" value="package-no" />
+								<label htmlFor="package-no">No</label>
+							</td>
+						</tr>
+						<tr>
+							<th style="padding-top: 1em;">Your Email Address:</th>
+							<td style="vertical-align: bottom; padding-top: 1em;">
+								<input type="email" id="customerEmail" />
+							</td>
+						</tr>
+						<tr>
+							<th style="padding-top: 1em;">Message:</th>
+							<td style="vertical-align: bottom; padding-top: 1em;">
+								<textarea rows="3" cols="30" />
+							</td>
+						</tr> */}
+					</tbody>
+				</table>
+				{/* <a
+					className="button-style"
+					id="input-submitAS"
+					type="button"
+					href="javascript:void(0);"
+					onClick="submitCustom()"
+				>
+					Confirm
+				</a> */}
 			</div>
 		</div>
 	);
